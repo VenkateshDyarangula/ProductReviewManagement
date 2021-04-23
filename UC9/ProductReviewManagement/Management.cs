0629d23cdec006c4b63655ce98618213a45040ef
venkatesh179 <venkateshdyarangula@gmail.com>
2021-04-23 08:38:37 +0530

    All UC's Pushing at a time

---
+++ UC9/ProductReviewManagement/Management.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReviews in listProductReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3);
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProducID + " " + "UserID:- " + list.UserID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            }
+
+        }
+
+        public void SelectedRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = from productReviews in listProductReview
+                               where (productReviews.ProducID == 1 || productReviews.ProducID == 4 || productReviews.ProducID == 9)
+                               && productReviews.Rating > 3
+                               select productReviews;
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProducID + " " + "UserID:- " + list.UserID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            }
+
+        }
+
+
+        public void RetrieveCountOfRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = listProductReview.GroupBy(x => x.ProducID).Select(x => new { ProductID = x.Key, Count = x.Count() });
+
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine(list.ProductID + "--------" + list.Count);
+
+            }
+        }
+        
+              public void RetrieveProductIdAndReview(List<ProductReview> listProductReview)
+              {
+                 var recordedData =  listProductReview.Select(item => new ProductReview
+                                        {
+                                       ProducID = item.ProducID,
+                                       Review = item.Review,
+                                      }
+                                       ).ToList();
+
+
+                 foreach (var list in recordedData)
+                 {
+               Console.WriteLine(list.ProducID + "--------" +list.Review);
+
+                 }
+            }
+
+
+           public void skipTopFiveRecords(List<ProductReview> listProductReview)
+           {
+            var recordedData = (from productReviews in listProductReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Skip(5);
+
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProducID + " " + "UserID:- " + list.UserID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            }
+        }
+
+       
+
+
+    }
+}
